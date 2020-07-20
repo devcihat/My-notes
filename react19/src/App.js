ca19aa7b3f522e191849344fec7df3eee271b89c
devcihat <cihattsungu@outlook.com>
2020-07-20 21:54:51 +0300

    my notes

---
+++ react19/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./redux/createStore";
+import InputSection from "./components/InputSection";
+import NotesSection from "./components/NotesSection";
+import "./App.scss";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="container">
+        <h1>My Notes</h1>
+        <InputSection />
+        <div className="line"></div>
+        <NotesSection />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ react19/src/components/NoteItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NoteItem.style.scss";
+const NoteItem = ({ title, content, onItemClicked }) => {
+  return (
+    <div className="NoteItem__container" role="button" onClick={onItemClicked}>
+      <h2>{title}</h2>
+      <p>{content}</p>
+    </div>
+  );
+};
+
+export default NoteItem;
